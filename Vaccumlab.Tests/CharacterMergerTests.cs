e2d6b753fcf3b658dcffad4693187ac4f4941b0a
Paweł Haracz <pawel.haracz.93@gmail.com>
2022-09-14 16:10:00 +0200

    add other test to check only vowels and consonants

--- Vaccumlab.Tests/CharacterMergerTests.cs
+++ Vaccumlab.Tests/CharacterMergerTests.cs
@@ -24,6 +24,7 @@ public class CharacterMergerTests
     [InlineData("abba", "aba")]
     [InlineData("abwrdta", "aba")]
     [InlineData("bdabwrdta", "baba")]
+    [InlineData("bcdfghjklmnpqrstvwxz", "b")]
     public void word_contains_group_of_consecutive_consonants_should_be_merged(string word, string expected)
     {
         var act = Act(word);
@@ -34,6 +35,7 @@ public class CharacterMergerTests
     [InlineData("aaba", "aba")]
     [InlineData("bya", "ba")]
     [InlineData("naomi", "nomi")]
+    [InlineData("aeiouy", "y")]
     public void word_contains_group_of_consecutive_should_be_merged_to_last_vowel(string word, string expected)
     {
         var act = Act(word);
--- Vaccumlab/Implementations/CharacterMerger.cs
+++ Vaccumlab/Implementations/CharacterMerger.cs
@@ -28,7 +28,7 @@ internal class CharacterMerger : ICharacterMerger
                     character = nextCharacter;
                     i = j;
                 }
-                if (Constants.Consonants.Contains(currentCharacter) && Constants.Consonants.Contains(nextCharacter))
+                else if (Constants.Consonants.Contains(currentCharacter) && Constants.Consonants.Contains(nextCharacter))
                 {
                     i = j;
                 }
@@ -43,49 +43,4 @@ internal class CharacterMerger : ICharacterMerger
 
         return sb.ToString();
     }
-    
-    // public string Merge(string word)
-    // {
-    //     var sb = new StringBuilder();
-    //
-    //     for (int i = 0; i < word.Length; i++)
-    //     {
-    //         if (Constants.Vowels.Contains(word[i]))
-    //         {
-    //             var vowel = word[i];
-    //             for (int j = i + 1; j < word.Length; j++)
-    //             {
-    //                 if (Constants.Vowels.Contains(word[j]))
-    //                 { 
-    //                     vowel = word[j];
-    //                     i = j;
-    //                     break;
-    //                 }
-    //                 
-    //             }
-    //             sb.Append(vowel);
-    //         }
-    //         else
-    //         {
-    //             for (int j = i + 1; j < word.Length; j++)
-    //             {
-    //                 //skip all next Consonants
-    //                 if (Constants.Vowels.Contains(word[j]))
-    //                 {
-    //                     sb.Append(word[i]);
-    //                     i = j-1;
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //
-    //     // var lastCharacters = word.Last();
-    //     // if (Constants.Vowels.Contains(lastCharacters))
-    //     // {
-    //     //     sb.Append(lastCharacters);
-    //     // }
-    //     //
-    //     return sb.ToString();
-    // }
 }
